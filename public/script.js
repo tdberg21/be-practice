e6305dc39035f8ca9230d05674abe610fde6d092
Tory Dannenberg <rfdberg@gmail.com>
2018-08-13 15:08:57 -0600

    fix button

--- public/script.js
+++ public/script.js
@@ -1,7 +1,8 @@
 const button = document.querySelector('button');
 
-button.addEventListener('click', doTheThings);
-
 const doTheThings = () => {
   console.log('hello world')
 }
+
+button.addEventListener('click', doTheThings);
+
